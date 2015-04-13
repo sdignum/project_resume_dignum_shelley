f145e354cf0f19ac5da4ec8c625b34055a250b78
Richard Kalehoff <richardkalehoff@gmail.com>
2015-04-13 08:23:02 -0400

    convert 'onclick' in HTML file to jQuery click handler in JS file

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
 	$('.sticky').positionSticky();
 
 	$('body').animatescroll();
+
+	$( '#jump-to-education' ).on( 'click', function() {
+		$('#section4').animatescroll({scrollSpeed:2000,easing:'easeInOutBack'});
+	});
 })
