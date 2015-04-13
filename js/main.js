e4f0caf51904c04a1de7a57b940d18305f8cb99e
Richard Kalehoff <richardkalehoff@gmail.com>
2015-04-13 08:31:42 -0400

    make animatescroll nav links work generically

--- js/main.js
+++ js/main.js
@@ -6,4 +6,10 @@ $(document).ready(function(){
 	$( '#jump-to-education' ).on( 'click', function() {
 		$('#section4').animatescroll({scrollSpeed:2000,easing:'easeInOutBack'});
 	});
+
+	$( '.jump-to-section' ).on( 'click', function() {
+		var section = $(this).attr('data-section');
+
+		$( section ).animatescroll({scrollSpeed:2000,easing:'easeInOutBack'});
+	});
 })
